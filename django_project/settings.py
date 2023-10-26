d9e2073fc004073d8662e5a876af062875a7777d
Jonathan Haberl <haberljonathan@gmail.com>
2023-10-26 10:57:06 -0600

    Chapter 14

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'accounts',
-    'pages'
+    'pages',
+    'articles'
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "boostrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+TIME_ZONE = "America/Denver"
